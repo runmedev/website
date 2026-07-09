c23339bac4d8017a4be9ed2960ab399d7a2ed143
Sebastian (Tiedtke) Huckleberry <sebastiantiedtke@gmail.com>
2026-07-09 10:54:28 -0700

    Restore GitHub star count
    Signed-off-by: Sebastian (Tiedtke) Huckleberry <sebastiantiedtke@gmail.com>

--- components/GitHubStars.tsx
+++ components/GitHubStars.tsx
@@ -1,6 +1,9 @@
+"use client";
+
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
 import { faStar } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useEffect, useState } from "react";
 
 interface GitHubStarsProps {
   url: string;
@@ -8,6 +11,37 @@ interface GitHubStarsProps {
 }
 
 const GitHubStars: React.FC<GitHubStarsProps> = ({ url, text }) => {
+  const [stars, setStars] = useState<string | null>(null);
+
+  useEffect(() => {
+    const controller = new AbortController();
+
+    fetch("https://sourishkrout-runmetotals.web.val.run/", {
+      signal: controller.signal,
+    })
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error("Failed to fetch Runme totals");
+        }
+
+        return res.json() as Promise<{ data?: { stars?: number } }>;
+      })
+      .then((resp) => {
+        const starCount = resp.data?.stars;
+
+        if (typeof starCount === "number") {
+          setStars(formatStars(starCount));
+        }
+      })
+      .catch((error: Error) => {
+        if (error.name !== "AbortError") {
+          console.error(error);
+        }
+      });
+
+    return () => controller.abort();
+  }, []);
+
   return (
     <a
       href={url}
@@ -17,8 +51,21 @@ const GitHubStars: React.FC<GitHubStarsProps> = ({ url, text }) => {
       <FontAwesomeIcon icon={faGithub} />
       <span>Star</span>
       <FontAwesomeIcon icon={faStar} className="text-yellow-500" />
+      {stars && (
+        <span className="border-l border-neutral-300 pl-2 tabular-nums" aria-label={`${stars} GitHub stars`}>
+          {stars}
+        </span>
+      )}
     </a>
   );
 };
+
+function formatStars(stars: number): string {
+  if (stars < 1000) {
+    return stars.toString();
+  }
+
+  return `${Math.round(stars / 100) / 10}k`;
+}
 
 export default GitHubStars;
