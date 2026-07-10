fd3deb820196be78e0ec03e7d3dd25a21de3c780
Sebastian (Tiedtke) Huckleberry <sebastiantiedtke@gmail.com>
2026-07-10 15:23:36 -0700

    Adjust blog hero cover height

--- app/(root)/blog/[slug]/page.tsx
+++ app/(root)/blog/[slug]/page.tsx
@@ -133,7 +133,7 @@ export default async function BlogSlug({ params }: PageParams) {
             </div>
           </div>
 
-          <div className="grid min-w-0 items-center gap-8 md:grid-cols-[minmax(0,1fr)_420px] md:gap-6">
+          <div className="grid min-w-0 items-center gap-8 md:grid-cols-[minmax(0,1fr)_500px] md:gap-6">
             <div className="flex min-w-0 flex-col gap-6">
               <div className="flex flex-row items-center space-x-3">
                 <div>
@@ -160,12 +160,12 @@ export default async function BlogSlug({ params }: PageParams) {
             </div>
 
             {post.cover?.url && (
-              <div className="relative min-w-0 overflow-visible md:-mr-8">
+              <div className="relative h-64 min-w-0 overflow-visible md:-mr-8 md:h-auto">
                 <img
                   src={post.cover.url}
                   alt=""
                   aria-hidden="true"
-                  className="h-auto w-full object-contain object-right opacity-80 [mask-image:linear-gradient(90deg,transparent_0%,black_18%,black_68%,transparent_100%)] [-webkit-mask-image:linear-gradient(90deg,transparent_0%,black_18%,black_68%,transparent_100%)]"
+                  className="absolute right-0 top-1/2 h-full w-auto max-w-none -translate-y-1/2 opacity-80 [mask-image:linear-gradient(90deg,transparent_0%,black_30%,black_70%,transparent_100%)] [-webkit-mask-image:linear-gradient(90deg,transparent_0%,black_30%,black_70%,transparent_100%)] md:static md:h-auto md:w-full md:max-w-full md:translate-y-0 md:[mask-image:linear-gradient(90deg,transparent_0%,black_18%,black_68%,transparent_100%)] md:[-webkit-mask-image:linear-gradient(90deg,transparent_0%,black_18%,black_68%,transparent_100%)]"
                 />
               </div>
             )}
